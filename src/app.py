e1c401ac9bc57c10cc8eaa5eb93397223194d2fe
Tan Suryadi Irvan <tirvan@redhat.com>
2024-07-27 18:17:01 +0800

    chore: update app.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world - v2.0"
+    statement = os.environ['APPENV'] + ": Hello world - v3.0\n\n"
+    return statement
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
